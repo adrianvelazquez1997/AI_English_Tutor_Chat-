47ecc774eb548728bd9e03b4b35db6997c0acf6a
Adrian Velazquez <adrianvelazquez.unrn@gmail.com>
2026-04-19 14:00:49 -0300

    Agregollamdo del embedding

---
+++ src/embeddings.py
@@ -0,0 +1,23 @@
+import os 
+from dotenv import load_dotenv 
+from openai import OpenAI
+
+load_dotenv()
+
+openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+EMBEDDING_MODEL = "text-embedding-3-small"
+
+def embed_text(text:str) -> list[float]:
+    response = openai.embeddings.create(
+        model = EMBEDDING_MODEL,
+        input = text
+    )
+    return response.data[0].embedding
+
+def embed_texts(text:str) -> list[list[float]]:
+    response = client.embeddings.create(
+        model=EMBEDDING_MODEL,
+        input=texts
+    )
+    return [item.embedding for item in response.data]
+
